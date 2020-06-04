a6a7f38eedcaa577c8a5738a43d0112860ea6438
ugurekmekci <36724485+ugurekmekci@users.noreply.github.com>
2020-06-04 21:45:49 +0200

    Update app.py

--- web/app.py
+++ web/app.py
@@ -19,8 +19,11 @@ from models import *
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        name = request.form['name']
+        job = request.form['job']
         text = request.form['text']
-        post = Post(text)
+
+        post = Post(text + " " + name + " " + job)
         db.session.add(post)
         db.session.commit()
     posts = Post.query.order_by(Post.date_posted.desc()).all()
